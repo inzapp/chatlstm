0cdc3cf53d0daffdb0d5fe726c77eb06f0911c35
inzapp <inzapp@naver.com>
2025-03-18 18:03:02 +0900

    remove bn in conv layer, use second lstm layer and use all sequence unit, change kernel initializer glorot to he

--- model.py
+++ model.py
@@ -55,6 +55,7 @@ class Model:
             x = self.gru(x, units=self.cfg.recurrent_units)
         else:
             x = self.lstm(x, units=self.cfg.recurrent_units)
+        x = tf.keras.layers.Flatten()(x)
         output_layer = self.output_layer(x)
         model = tf.keras.models.Model(input_layer, output_layer)
         return model
@@ -67,39 +68,25 @@ class Model:
             kernel_size=kernel_size,
             kernel_initializer=self.kernel_initializer(),
             kernel_regularizer=self.kernel_regularizer())(x)
-        x = tf.keras.layers.BatchNormalization()(x)
         return self.activation(x, activation)
 
-    def lstm(self, x, units, initial_state=None, return_state=False):
-        if return_state:
-            x, state_h, state_c = tf.keras.layers.LSTM(units=units, return_state=return_state)(x, initial_state=initial_state)
-            return x, [state_h, state_c]
-        else:
-            x = tf.keras.layers.LSTM(units=units, return_state=return_state)(x, initial_state=initial_state)
-            return x
+    def lstm(self, x, units):
+        x = tf.keras.layers.LSTM(units=self.cfg.recurrent_units, return_sequences=True)(x)
+        x = tf.keras.layers.LSTM(units=64, return_sequences=True)(x)
+        return x
 
-    def gru(self, x, units, initial_state=None, return_state=False):
-        if return_state:
-            x, state_h = tf.keras.layers.GRU(units=units, return_state=return_state)(x, initial_state=initial_state)
-            return x, [state_h]
-        else:
-            x = tf.keras.layers.GRU(units=units, return_state=return_state)(x, initial_state=initial_state)
-            return x
-
-    def calculate_units(self, c=3, min_units=64, max_units=1024):
-        return min(max(int(c * math.sqrt(self.vocab_size)), min_units), max_units)
+    def gru(self, x, units):
+        x = tf.keras.layers.GRU(units=self.cfg.recurrent_units, return_sequences=True)(x)
+        x = tf.keras.layers.GRU(units=64, return_sequences=True)(x)
+        return x
 
     def output_layer(self, x):
-        # x = tf.keras.layers.Dense(units=self.calculate_units(), kernel_initializer=self.kernel_initializer())(x)
-        # x = tf.keras.layers.Dense(units=1024, kernel_initializer=self.kernel_initializer())(x)
-        # x = tf.keras.layers.BatchNormalization()(x)
-        # x = self.activation(x, 'leaky')
         if self.cfg.dropout < 0.0 <= 1.0:
             x = tf.keras.layers.Dropout(self.cfg.dropout)(x)
         return tf.keras.layers.Dense(units=self.vocab_size, kernel_initializer=self.kernel_initializer(), activation='softmax')(x)
 
     def kernel_initializer(self):
-        return tf.keras.initializers.glorot_normal()
+        return tf.keras.initializers.he_normal()
 
     def kernel_regularizer(self, l2=0.0005):
         return tf.keras.regularizers.l2(l2=l2)
